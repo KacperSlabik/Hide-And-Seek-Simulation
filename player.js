967b40fbd8bb2f9b0b52a7f2bd07519954ec6370
KacperSlabik <kacper.slabik2000@gmail.com>
2024-05-08 14:08:45 +0200

    air commit

--- player.js
+++ player.js
@@ -1,58 +1,69 @@
 // Klasa Player
 export class Player {
-	constructor(x, y, speedX, speedY, type, color) {
-		this.x = x;
-		this.y = y;
-		this.speedX = speedX; // Przechowuje szybkość ruchu gracza
-		this.speedY = speedY; // Przechowuje szybkość ruchu gracza
-		this.type = type;
-		this.color = color;
-		this.directionTimer = 0; // Timer do śledzenia czasu od ostatniej zmiany kierunku
-		this.changeDirectionInterval = Math.random() * 2000 + 1000; // Losowy czas między zmianami kierunku (od 1s do 3s)
-		this.directions = ['up', 'down', 'left', 'right']; // Dostępne kierunki
-		this.currentDirection = this.randomDirection(); // Losowy kierunek początkowy
-	}
+  constructor(x, y, speedX, speedY, type, color) {
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX; // Przechowuje szybkość ruchu gracza
+    this.speedY = speedY; // Przechowuje szybkość ruchu gracza
+    this.color = color;
+    this.directionTimer = 0; // Timer do śledzenia czasu od ostatniej zmiany kierunku
+    this.changeDirectionInterval = Math.random() * 2000 + 1000; // Losowy czas między zmianami kierunku (od 1s do 3s)
+    this.directions = ["up", "down", "left", "right"]; // Dostępne kierunki
+    this.currentDirection = this.randomDirection(); // Losowy kierunek początkowy
+  }
 
-	move(dx, dy) {
-		this.x += dx;
-		this.y += dy;
-	}
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
 
-	movePlayer() {
-		this.directionTimer += 16; // 16ms to czas między klatkami w animacji (przy 60 FPS)
+  movePlayer() {
+    this.directionTimer += 16; // 16ms to czas między klatkami w animacji (przy 60 FPS)
 
-		// Jeśli upłynął czas na zmianę kierunku
-		if (this.directionTimer >= this.changeDirectionInterval) {
-			this.directionTimer = 0; // Zresetuj timer
-			this.changeDirectionInterval = Math.random() * 4000 + 1000; // Wygeneruj nowy losowy czas między zmianami kierunku
-			this.currentDirection = this.randomDirection(); // Losowy nowy kierunek
-		}
+    // Jeśli upłynął czas na zmianę kierunku
+    if (this.directionTimer >= this.changeDirectionInterval) {
+      this.directionTimer = 0; // Zresetuj timer
+      this.changeDirectionInterval = Math.random() * 4000 + 1000; // Wygeneruj nowy losowy czas między zmianami kierunku
+      this.currentDirection = this.randomDirection(); // Losowy nowy kierunek
+    }
 
-		// Określ kierunek ruchu na podstawie aktualnego kierunku
-		let dx = 0;
-		let dy = 0;
-		switch (this.currentDirection) {
-			case 'up':
-				dy = -this.speedY;
-				break;
-			case 'down':
-				dy = this.speedY;
-				break;
-			case 'left':
-				dx = -this.speedX;
-				break;
-			case 'right':
-				dx = this.speedX;
-				break;
-		}
+    // Określ kierunek ruchu na podstawie aktualnego kierunku
+    let dx = 0;
+    let dy = 0;
+    switch (this.currentDirection) {
+      case "up":
+        dy = -this.speedY;
+        break;
+      case "down":
+        dy = this.speedY;
+        break;
+      case "left":
+        dx = -this.speedX;
+        break;
+      case "right":
+        dx = this.speedX;
+        break;
+    }
 
-		// Wykonaj ruch
-		this.move(dx, dy);
-	}
+    // Wykonaj ruch
+    this.move(dx, dy);
+  }
 
-	// Metoda do losowania nowego kierunku ruchu
-	randomDirection() {
-		const index = Math.floor(Math.random() * this.directions.length);
-		return this.directions[index];
-	}
+  runAwayFrom(seekerX, seekerY) {
+    const dx = this.x - seekerX;
+    const dy = this.y - seekerY;
+    if (Math.abs(dx) > Math.abs(dy)) {
+      // Poruszaj się w poziomie, aby oddalić się od seeker'a
+      this.currentDirection = dx > 0 ? "right" : "left";
+    } else {
+      // Poruszaj się w pionie, aby oddalić się od seeker'a
+      this.currentDirection = dy > 0 ? "down" : "up";
+    }
+  }
+
+  // Metoda do losowania nowego kierunku ruchu
+  randomDirection() {
+    const index = Math.floor(Math.random() * this.directions.length);
+    return this.directions[index];
+  }
 }
